f06817a2329c0ed610992c2f97cb8f157d2dcc48
lilp1ka <zaloznyy2006@gmail.com>
2024-10-02 01:39:01 +0200

    rewrite active_window; is_afk

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,86 +1,104 @@
-use x11::xlib::*;
-use std::ffi::{CStr, CString};
-use std::ptr;
+use std::thread;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use active_win_pos_rs::{get_active_window, ActiveWindow};
+use device_query::{device_events, DeviceEvents, DeviceQuery, DeviceState, KeyboardCallback, MousePosition};
 
 fn main() {
-    unsafe {
-        let display = XOpenDisplay(ptr::null());
-        if display.is_null() {
-            panic!("Failed to connect to X Server");
+    is_afk();
+    //thread 1 (is afk):
+        //monitoring device input
+        //logs this into LastState
+    //thread 2 (active window):
+        //getting active window
+        // if LastState.duration < 300s 
+            //logs this into ActiveApp
+        //else mark as afk
+    //thread 3 (send logs):
+        //sending logs every 5(?) mins
+    
+    //thread 1 uses:
+        //last state arc mutex
+    //thread 2 uses:
+        //active window arc mutex
+        //logs vec arc mutex
+        //last state arc
+    //thread 3 uses:
+        //logs vec arc
+}
+
+#[derive (Debug)]
+pub struct ActiveApp{
+    name: String,
+    title: String,
+    time: SystemTime,
+    duration: Duration
+}
+#[derive(Debug)]
+pub struct LastState{
+    coords: MousePosition,
+    time: SystemTime,
+    duration: Duration
+}
+
+fn is_afk(){
+    let device_state = DeviceState::new();
+
+    let mut last_state = LastState{
+        coords: device_state.get_mouse().coords,
+        time: SystemTime::now(),
+        duration: Duration::from_secs(0),
+    };
+    loop{
+            if !device_state.get_keys().is_empty() || 
+                device_state.get_mouse().coords != last_state.coords || 
+                device_state.get_mouse().button_pressed.contains(&true){
+                    last_state.coords = device_state.get_mouse().coords;
+                    last_state.duration = last_state.time.elapsed().unwrap();
+                    last_state.time = SystemTime::now();
+                    println!("{:#?} \n dur as sec: {}", last_state, last_state.duration.as_secs());
+                }
+            thread::sleep(Duration::from_millis(500));
         }
+}
 
-        let root = XDefaultRootWindow(display);
-        let atom = XInternAtom(display, CString::new("_NET_ACTIVE_WINDOW").unwrap().as_ptr(), 0);
+fn active_window(){
+    let mut logs: Vec<ActiveApp> = Vec::new(); //potom vinesti v nachalo potoka
+    let now = SystemTime::now(); //potom vinesti v nachalo potoka
+    let mut active_app = ActiveApp{
+                                    name: "None".to_string(),
+                                    title: "None".to_string(),
+                                    time: now,
+                                    duration: Duration::from_secs(0)
+                                    };
+    
+    println!("{:?}", now);
+    loop{
+        match get_active_window(){
+            Ok(window) => {
+                if active_app.name != window.app_name && active_app.title != window.title{
+                    log_active_app(&mut active_app);
 
-        let mut window: Window = 0;
-        let mut actual_type = 0;
-        let mut actual_format = 0;
-        let mut nitems = 0;
-        let mut bytes_after = 0;
-        let mut prop: *mut u8 = ptr::null_mut();
+                    active_app.name = window.app_name;
+                    active_app.title = window.title;
+                    active_app.time = SystemTime::now();
+                }
+                else if active_app.title != window.title {
+                    log_active_app(&mut active_app);
 
-        let status = XGetWindowProperty(
-            display,
-            root,
-            atom,
-            0,
-            1,
-            0,
-            AnyPropertyType as u64,
-            &mut actual_type,
-            &mut actual_format,
-            &mut nitems,
-            &mut bytes_after,
-            &mut prop,
-        );
-
-        if status != Success as i32 {
-            println!("Failed to get status: {}", status);
-        } else if nitems > 0 {
-            window = *(prop as *mut Window);
-            XFree(prop as *mut libc::c_void);
-            println!("Got active window: {:?}", window);
-        } else {
-            println!("Couldn't find active window");
-            XFree(prop as *mut libc::c_void);
-            return;
+                    active_app.title = window.title;
+                    active_app.time = SystemTime::now();
+                }
+                
+            },
+            Err(_) => {},
         }
-
-
-        let mut name: *mut i8 = ptr::null_mut();
-        if XFetchName(display, window, &mut name) != 0 && !name.is_null() {
-            let window_name = CStr::from_ptr(name).to_string_lossy().into_owned();
-            XFree(name as *mut libc::c_void);
-            println!("Active window name: {}", window_name);
-        } else {
-            println!("Didn't get active window name, checking other things...");
-
-            let mut wm_name: *mut u8 = ptr::null_mut();
-            let wm_atom = XInternAtom(display, CString::new("WM_NAME").unwrap().as_ptr(), 0);
-            let status = XGetWindowProperty(
-                display,
-                window,
-                wm_atom,
-                0,
-                100,
-                0,
-                AnyPropertyType.try_into().unwrap(),
-                &mut actual_type,
-                &mut actual_format,
-                &mut nitems,
-                &mut bytes_after,
-                &mut wm_name,
-            );
-
-            if status == Success as i32 && nitems > 0 && !wm_name.is_null() {
-                let wm_name_str = CStr::from_ptr(wm_name as *mut i8).to_string_lossy().into_owned();
-                XFree(wm_name as *mut libc::c_void);
-                println!("WM_NAME active window: {}", wm_name_str);
-            } else {
-                println!("couldnt get WM_NAME.");
-            }
-        }
-
-        XCloseDisplay(display);
+        thread::sleep(Duration::from_millis(500));
     }
+}
+
+fn log_active_app(app: &mut ActiveApp){
+    app.duration = app.time.elapsed().unwrap();
+    //dobavit' push v log list
+    println!("{:#?}, {}", app, app.duration.as_secs());
+
 }

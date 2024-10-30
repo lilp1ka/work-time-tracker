fa42e563d0d79597a23c1ad88e68f84c1838c5e8
space <68478800+PARADIGMASPACE@users.noreply.github.com>
2024-10-30 11:24:36 +0100

    52 нахуй + миграции

---
+++ backend/auth_service/database/models.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP, ForeignKey, UniqueConstraint
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(255), nullable=False, unique=True)
+    email = Column(String(255), nullable=False, unique=True)
+    hashed_password = Column(String(255), nullable=False)
+    is_active = Column(Boolean, default=False)
+    is_admin = Column(Boolean, default=False)
+    created_at = Column(TIMESTAMP, server_default=func.current_timestamp())
+
+
+class Token(Base):
+    __tablename__ = 'sessions'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    device_info = Column(String(255))
+    refresh_token = Column(String(512), nullable=False)
+    issued_at = Column(TIMESTAMP, server_default=func.current_timestamp())
+    expires_at = Column(TIMESTAMP, nullable=False)
+    valid = Column(Boolean, default=True)
